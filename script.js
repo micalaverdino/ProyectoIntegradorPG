c5290276e708fb45c487c0da486ad87db4a88c85
micalaverdino <micaelalaverdino@gmail.com>
2024-05-16 15:42:42 -0300

    modificacion de paginas, agregado de imagenes, cambios de nombres, etc

---
+++ script.js
@@ -0,0 +1,25 @@
+/*let nombre = 'Pedro';
+let years = 30;
+console.log(years + nombre);
+*/
+/*  Clase 10 Java Script
+let numberone=120;
+let numbertwo=20;
+let resultsuma = numberone + numbertwo;
+let resultresta = numberone - numbertwo;
+let resultmulti = numberone * numbertwo;
+let resultdivision = numberone / numbertwo;
+console.log("Suma", resultsuma);
+console.log("Resta", resultresta);
+console.log("Multiplicacion", resultmulti);
+console.log("División", resultdivision);
+*/
+/* Tarea Clase 10 */
+let nombre = "Lionel";
+let apellido = Messi;
+console.log(nombre + " " + apellido);
+let numberone=1996
+let numbertwo=1998
+let numberthree=223
+let resultsuma = numberone + numberone;
+console.log("Suma", resultsuma);
