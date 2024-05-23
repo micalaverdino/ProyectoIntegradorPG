9d1e949f56435c3debd8e360f12ac5748d825f69
micalaverdino <micaelalaverdino@gmail.com>
2024-05-22 21:00:21 -0300

    clase 12 tarea

--- script.js
+++ script.js
@@ -69,4 +69,85 @@ if(topping == "Oreo") {
 precioFinal = helado + precio;
 console.log("El helado cuesta $" + precioFinal)
 
+//CLASE 11 - TAREA
+let menu = ['pescado, carne, verdura'];
+for (let i = 0; i < menu.length; i++) {
+    let eleccion = menu [i];
 
+    switch (eleccion) {
+        case 'carne':
+            console.log('Si pide carne, se le ofrecerá como bebida vino tinto.');
+            break;
+        case 'pescado':
+            console.log('Si pide pescado, se le ofrecerá como bebida vino blanco.');
+            break;
+        case 'verdura':
+            console.log('Si pide verdura, se le ofrecerá como bebida agua.');
+            break;
+        default:
+            console.log('Elija carne, pescado o verdura');
+    }
+}
+// CLASE 12
+/*
+// Declarar variables
+let numero = 1;  // Número inicial
+let suma = 0;    // Variable para almacenar la suma
+
+// Bucle while
+while (numero <= 10) {
+  suma += numero;  // Sumar el valor actual de 'numero' a 'suma'
+  numero++;        // Incrementar 'numero' en 1
+}
+
+// Mostrar el resultado
+console.log("La suma de los números del 1 al 10 es: " + suma);
+
+let numeros = 8;
+let sumas = 0;
+
+while (numeros <=10) {
+    sumas += numeros;
+    numeros++;
+}
+
+    console.log("La sumas de los numeros del 1 al 10 es: " + sumas);
+
+let resultado
+let numero3 = 28;
+let numero4 = 30;
+function suma_y_muestra () {
+    resultado = numero3 + numero4;
+    alert("El resultado es " + resultado)
+}
+suma_y_muestra ();
+
+// Solicita al usuario que introduzca un número entero
+let numero = prompt("Introduce un número entero");
+
+// Llama a la función parImpar y guarda el resultado
+let resultado = parImpar(numero);
+
+// Muestra el resultado en una alerta
+alert("El número " + numero + " es " + resultado);
+
+// Define la función parImpar
+function parImpar(numero) {
+  if (numero % 2 == 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+}
+*/
+let nombre = prompt('Ingrese su nombre');
+function saludo() {
+    if (nombre) {
+        alert('Bienvenido ' + nombre);
+    }
+    else {
+        alert('Debe ingresar un nombre');
+    }
+}
+
+saludo();
