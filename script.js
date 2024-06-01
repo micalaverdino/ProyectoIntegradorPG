39d469e245b8a8d165d6f0fe7ffb283213a3154d
sebastian <sebastian.dorsch@crombie.dev>
2024-06-01 15:33:57 -0300

    cambios

--- script.js
+++ script.js
@@ -10,17 +10,17 @@ Multiplicacion_y_muestra(num); */
  // let nombre = prompt("Escribe tu tag");
 // alert("Bienvenido/a" + " " + nombre + " " + "a Punto Gamers");
 
-// function verificarEdad() {
-//     let edad = prompt("Ingrese su edad");
-//     if(isNaN(edad)) {
-//         alert("ingrese el numero");
-//     } else {
-//         edad = parseInt(edad);
-//         if(edad < 18){
-//             alert("Debe ingresar con un responsable");
-//         } else{
-//             alert("Bienvenidos a Punto Gamers");
-//         }
-//     }
-// }
-// window.onload = verificarEdad;
+function verificarEdad() {
+    let edad = prompt("Ingrese su edad");
+    if(isNaN(edad)) {
+        alert("ingrese el numero");
+    } else {
+        edad = parseInt(edad);
+        if(edad < 18){
+            alert("Debe ingresar con un responsable");
+        } else{
+            alert("Bienvenidos a Punto Gamers");
+        }
+    }
+}
+window.onload = verificarEdad;
