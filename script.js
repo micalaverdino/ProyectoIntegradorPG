14c22c103e6d00ecf3d3d4c4d81ca2e3b99e2b77
micalaverdino <micaelalaverdino@gmail.com>
2024-06-03 00:18:42 -0300

    modificaciones en footer y formulario

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-/*let num = prompt("¿Cuánto es 6*9?");
-function Multiplicacion_y_muestra (num) {
-    if(num == 54) {
-        alert("Bienvenido al sitio web");
-    } else {
-        alert("¡Ponete a estudiar!");
-    }
-}
-Multiplicacion_y_muestra(num); */
- // let nombre = prompt("Escribe tu tag");
-// alert("Bienvenido/a" + " " + nombre + " " + "a Punto Gamers");
+// Multiplicacion_y_muestra(num); */
+//  // let nombre = prompt("Escribe tu tag");
+// // alert("Bienvenido/a" + " " + nombre + " " + "a Punto Gamers");
 
 // function verificarEdad() {
 //     let edad = prompt("Ingrese su edad");
@@ -19,7 +11,7 @@ Multiplicacion_y_muestra(num); */
 //         if(edad < 18){
 //             alert("Debe ingresar con un responsable");
 //         } else{
-//             alert("Bienvenidos a Punto Gamers");
+//             alert("Bienvenido a Punto Gamers");
 //         }
 //     }
 // }
